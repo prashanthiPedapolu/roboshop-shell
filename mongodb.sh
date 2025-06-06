55eee74f398a911b995dc89be5063e2787b64ffb
prashanthiPedapolu <pedapolu.prashanthi24@gmail.com>
2025-06-06 19:46:10 -0400

    install mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,47 +1,54 @@
 #!/bin/bash
+
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/roboshopshell-log"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
+SCRIPT_NAME=$(basename "$0" | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-mkdir -p $LOGS_FOLDER
-echo "script started and executed at : $(date)" &>>$LOG_FILE
 
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N"
-    exit 1 #give other than 0 upto 127
+mkdir -p $LOGS_FOLDER
+echo "Script started and executed at: $(date)" &>> $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+  echo -e "$R ERROR: Please run this script with root access $N"
+  exit 1
 else
-    echo "You are running with root access"
+  echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-# validate functions takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "Installing $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "Installing $2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-    }
-    cp /tmp/mongo.repo /etc/yum.repos.d/mongod.repo &>>$LOG_FILE
-    VALIDATE $? "copying mongo db repo"
+# Validate function to check last command status
+VALIDATE() {
+  if [ $1 -eq 0 ]; then
+    echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+  else
+    echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+    exit 1
+  fi
+}
 
-    dnf install mongodb-org -y &>>$LOG_FILE
-    VALIDATE $? "Mongodb installing server"
+# Copy the repo file
+cp /tmp/mongo.repo /etc/yum.repos.d/mongod.repo &>> $LOG_FILE
+VALIDATE $? "Copying MongoDB repo file"
 
-    systemctl enable mongod &>>$LOG_FILE
-    VALIDATE $? "enabling mongod"
+# Install MongoDB
+dnf install -y mongodb-org &>> $LOG_FILE
+VALIDATE $? "Installing MongoDB server"
 
-    systemctl start mongod &>>$LOG_FILE
-    VALIDATE $? "starting mongod"
+# Enable MongoDB service
+systemctl enable mongod &>> $LOG_FILE
+VALIDATE $? "Enabling mongod service"
 
-    sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG_FILE
-    VALIDATE $? "changing bind ip in mongod.conf"
+# Start MongoDB service
+systemctl start mongod &>> $LOG_FILE
+VALIDATE $? "Starting mongod service"
 
-    systemctl restart mongod &>>$LOG_FILE
-    VALIDATE $? "Restarting  mogod"
+# Update MongoDB bind IP
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOG_FILE
+VALIDATE $? "Changing bind IP in mongod.conf"
+
+# Restart MongoDB to apply config
+systemctl restart mongod &>> $LOG_FILE
+VALIDATE $? "Restarting mongod service"
