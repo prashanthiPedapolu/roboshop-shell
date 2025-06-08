239fe279357e087cbf9b7db1d67f55b8fbc4fe4b
prashanthiPedapolu <pedapolu.prashanthi24@gmail.com>
2025-06-08 09:09:26 -0400

    instances creating

--- instances.sh
+++ instances.sh
@@ -7,7 +7,7 @@ ZONE_ID="Z04259892SI66LB8JS18Q"
 INSTANCE_TYPE="t2.micro"
 DOMAIN_NAME="mylearnings.site"
 
-for instance in "${INSTANCES[$@]}"
+for instance in $@
 do
 INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0ad2fa42882b8a978 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 
