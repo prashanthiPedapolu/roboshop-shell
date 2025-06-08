9be04a1aeaa0d9d6264e87525e4585651395b470
prashanthiPedapolu <pedapolu.prashanthi24@gmail.com>
2025-06-08 08:59:23 -0400

    instances creating

--- instances.sh
+++ instances.sh
@@ -7,15 +7,17 @@ ZONE_ID="Z04259892SI66LB8JS18Q"
 INSTANCE_TYPE="t2.micro"
 DOMAIN_NAME="mylearnings.site"
 
-for instance in "${INSTANCES[@]}"
+for instance in "${INSTANCES[$@]}"
 do
 INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0ad2fa42882b8a978 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 
     if [ $instance != "frontend" ]
     then
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    RECORD_NAME="$instance.$DOMAIN_NAME"
     else
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    RECORD_NAME="$instance.$DOMAIN_NAME"
     fi
     echo "$instance IP Address: $IP"
 
@@ -26,7 +28,7 @@ INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-
     \"Changes\": [{
       \"Action\": \"UPSERT\",
       \"ResourceRecordSet\": {
-        \"Name\": \"$instance.$DOMAIN_NAME\",
+        \"Name\": \"$RECORD_NAME\",
         \"Type\": \"A\",
         \"TTL\": 300,
         \"ResourceRecords\": [{\"Value\": \"$IP\"}]
