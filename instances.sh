59fbebd268d53433ee773096667e44a80556b4a8
prashanthiPedapolu <pedapolu.prashanthi24@gmail.com>
2025-06-26 21:23:36 -0400

    updating instances

--- instances.sh
+++ instances.sh
@@ -16,3 +16,4 @@ INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
     fi
     echo "$instance IP Address: $IP"
+    done
